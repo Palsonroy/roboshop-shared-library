6f6de09faf1c3b37e67075a089a054930cdd7116
Palsonroy <Palsonroy123@gmail.com>
2024-09-16 21:22:26 +0530

    jenkins

---
+++ vars/nodejsVM.groovy
@@ -0,0 +1,119 @@
+def call(Map configMap){
+    pipeline {
+        agent {
+        node {
+            label 'AGENT'
+            
+        }
+    }
+    environment { 
+            packageVersion = ''
+            nexusUrl = '172.31.87.249:8081'
+        }
+    options {
+        // Timeout counter starts AFTER agent is allocated
+        timeout(time: 1 , unit: 'HOURS')
+        disableConcurrentBuilds()
+    }
+     parameters {
+        // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+
+        // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+
+        booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+
+        // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+
+        // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+    }
+    
+        stages {
+            stage('Get the version') {
+                steps {
+                    script { //groovy script
+                        def packageJson = readJSON file: 'package.json' //def is variable initialisation
+                        packageVersion = packageJson.version
+                        echo "application version: $packageVersion"
+                    }
+                }
+            }
+            //npm install to install dependencies
+             stage('install dependencies') {
+                steps {
+                    sh """
+                        npm install
+                    """
+                }
+            }
+             stage('Unit tests') {
+                steps {
+                    sh """
+                        echo "unit tests will run here"
+                    """  
+                }
+            }
+             stage('Sonar Scan') {
+                steps {
+                    sh """
+                       sonar-scanner 
+                    """  
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh """
+                        ls -la
+                         zip -q -r catalogue.zip ./* -x ".git" -x "*.zip"
+                         ls -ltr
+                    """  
+                }
+            }
+            stage('Publish Artifacts') {
+                steps {
+                      nexusArtifactUploader(
+                                nexusVersion: 'nexus3',
+                                protocol: 'http',
+                                nexusUrl: "${nexusUrl}",
+                                groupId: 'com.roboshop',
+                                version: "${packageVersion}",
+                                repository: 'catalogue',
+                                credentialsId: 'nexus-auth',
+                                artifacts: [
+                                    [artifactId: 'catalogue',
+                                    classifier: '',
+                                    file: 'catalogue.zip' ,
+                                    type: 'zip']
+                                ]
+                         )
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression {
+                    params.Deploy == 'true'
+                    }
+                }
+                steps {
+                     script {
+                        def params =[
+                            string(name: 'version', value: "$packageVersion"),
+                             string(name: 'environment', value: "dev")
+                        ]
+                        build job: "catalogue-deploy", wait: true, parameters: params
+                    }
+
+                }
+            }
+    
+        
+        }
+
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+        }
+    }
+
+}
