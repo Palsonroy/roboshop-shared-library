2b8dc2413c28793d282de1c10a07f340d3939fa6
Palsonroy <Palsonroy123@gmail.com>
2024-09-26 13:54:04 +0530

    deploy

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -6,88 +6,203 @@ def call(Map configMap){
             
         }
     }
-    environment { 
-            packageVersion = ''
-            //can maintain in pipelineGlobals
-          //  nexusUrl = '172.31.87.249:8081'
-        }
-    options {
-        // Timeout counter starts AFTER agent is allocated
-        timeout(time: 1 , unit: 'HOURS')
-        disableConcurrentBuilds()
-    }
-     parameters {
-        // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+//     environment { 
+//             packageVersion = ''
+//             //can maintain in pipelineGlobals
+//           //  nexusUrl = '172.31.87.249:8081'
+//         }
+//     options {
+//         // Timeout counter starts AFTER agent is allocated
+//         timeout(time: 1 , unit: 'HOURS')
+//         disableConcurrentBuilds()
+//     }
+//      parameters {
+//         // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
 
-        // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+//         // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
 
-        booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+//         booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
 
-        // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+//         // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
 
-        // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-    }
+//         // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+//     }
     
+//         stages {
+//             stage('Get the version') {
+//                 steps {
+//                     script { //groovy script
+//                         def packageJson = readJSON file: 'package.json' //def is variable initialisation
+//                         packageVersion = packageJson.version
+//                         echo "application version: $packageVersion"
+//                     }
+//                 }
+//             }
+//             //npm install to install dependencies
+//              stage('install dependencies') {
+//                 steps {
+//                     sh """
+//                         npm install
+//                     """
+//                 }
+//             }
+//              stage('Unit tests') {
+//                 steps {
+//                     sh """
+//                         echo "unit tests will run here"
+//                     """  
+//                 }
+//             }
+//              stage('Sonar Scan') {
+//                 steps {
+//                     sh """
+//                          echo " usually here we will use the command is sonar-scanner "
+//                          echo "sonar scan will run here"
+
+//                     """  
+//                 }
+//             }
+//             stage('Build') {
+//                 steps {
+//                     sh """
+//                         ls -la
+//                          zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
+//                          ls -ltr
+//                     """  
+//                 }
+//             }
+//             stage('Publish Artifacts') {
+//                 steps {
+//                       nexusArtifactUploader(
+//                                 nexusVersion: 'nexus3',
+//                                 protocol: 'http',
+//                                 nexusUrl: pipelineGlobals.nexusUrl(),
+//                                 groupId: 'com.roboshop',
+//                                 version: "${packageVersion}",
+//                                 repository: "${configMap.component}",
+//                                 credentialsId: 'nexus-auth',
+//                                 artifacts: [
+//                                     [artifactId: "${configMap.component}",
+//                                     classifier: '',
+//                                     file: "${configMap.component}.zip" ,
+//                                     type: 'zip']
+//                                 ]
+//                          )
+//                 }
+//             }
+//             stage('Deploy') {
+//                 when {
+//                     expression{
+//                         params.Deploy
+//                     }
+//                 }
+//                 steps {
+//                     script {
+//                             def buildParams = [
+//                                 string(name: 'version', value: "$packageVersion"),
+//                                 string(name: 'environment', value: "dev")
+//                                 booleanParam(name: 'Create', value: params.Deploy)
+//                             ]
+//                             build job: "../${configMap.component}-deploy", wait: true, parameters: buildParams
+//                         }
+//                 }
+//             }
+//         }
+//         // post build
+//         post { 
+//             always { 
+//                 echo 'I will always say Hello again!'
+//                 deleteDir()
+//             }
+//             failure { 
+//                 echo 'this runs when pipeline is failed, used generally to send some alerts'
+//             }
+//             success{
+//                 echo 'I will say Hello when pipeline is success'
+//             }
+//         }
+//     }
+// }
+
+environment { 
+            packageVersion = ''
+            // can maintain in pipeline globals
+            //nexusURL = '172.31.5.95:8081'
+        }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+        }
+        parameters {
+            // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+
+            // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+
+            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+
+            // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+
+            // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+        }
+        // build
         stages {
             stage('Get the version') {
                 steps {
-                    script { //groovy script
-                        def packageJson = readJSON file: 'package.json' //def is variable initialisation
+                    script {
+                        def packageJson = readJSON file: 'package.json'
                         packageVersion = packageJson.version
                         echo "application version: $packageVersion"
                     }
                 }
             }
-            //npm install to install dependencies
-             stage('install dependencies') {
+            stage('Install dependencies') {
                 steps {
                     sh """
                         npm install
                     """
                 }
             }
-             stage('Unit tests') {
+            stage('Unit tests') {
                 steps {
                     sh """
                         echo "unit tests will run here"
-                    """  
+                    """
                 }
             }
-             stage('Sonar Scan') {
-                steps {
+            stage('Sonar Scan'){
+                steps{
                     sh """
-                         echo " usually here we will use the command is sonar-scanner "
-                         echo "sonar scan will run here"
-
-                    """  
+                        echo "usually command here is sonar-scanner"
+                        echo "sonar scan will run here"
+                    """
                 }
             }
             stage('Build') {
                 steps {
                     sh """
                         ls -la
-                         zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
-                         ls -ltr
-                    """  
+                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
+                        ls -ltr
+                    """
                 }
             }
-            stage('Publish Artifacts') {
+            stage('Publish Artifact') {
                 steps {
-                      nexusArtifactUploader(
-                                nexusVersion: 'nexus3',
-                                protocol: 'http',
-                                nexusUrl: pipelineGlobals.nexusUrl(),
-                                groupId: 'com.roboshop',
-                                version: "${packageVersion}",
-                                repository: "${configMap.component}",
-                                credentialsId: 'nexus-auth',
-                                artifacts: [
-                                    [artifactId: "${configMap.component}",
-                                    classifier: '',
-                                    file: "${configMap.component}.zip" ,
-                                    type: 'zip']
-                                ]
-                         )
+                    nexusArtifactUploader(
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: pipelineGlobals.nexusURL(),
+                        groupId: 'com.roboshop',
+                        version: "${packageVersion}",
+                        repository: "${configMap.component}",
+                        credentialsId: 'nexus-auth',
+                        artifacts: [
+                            [artifactId: "${configMap.component}",
+                            classifier: '',
+                            file: "${configMap.component}.zip",
+                            type: 'zip']
+                        ]
+                    )
                 }
             }
             stage('Deploy') {
@@ -98,12 +213,12 @@ def call(Map configMap){
                 }
                 steps {
                     script {
-                            def buildParams = [
+                            def params = [
                                 string(name: 'version', value: "$packageVersion"),
                                 string(name: 'environment', value: "dev")
-                                booleanParam(name: 'Create', value: params.Deploy)
+                                booleanParam(name: 'Create', value: "${params.Deploy}")
                             ]
-                            build job: "../${configMap.component}-deploy", wait: true, parameters: buildParams
+                            build job: "../${configMap.component}-deploy", wait: true, parameters: params
                         }
                 }
             }
@@ -121,5 +236,3 @@ def call(Map configMap){
                 echo 'I will say Hello when pipeline is success'
             }
         }
-    }
-}
